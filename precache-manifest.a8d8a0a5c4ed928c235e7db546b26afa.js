f05ee42d412b930d898fc8ae355d836f26279305
Mandeep2001 <39806321+Mandeep2001@users.noreply.github.com>
2019-11-20 21:11:45 +0100

    Updates

---
+++ precache-manifest.a8d8a0a5c4ed928c235e7db546b26afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ced1c090536ca8155ca14f39a4bdc9c",
+    "url": "/realtime-twitch/index.html"
+  },
+  {
+    "revision": "e144150840eaf2ee2ad4",
+    "url": "/realtime-twitch/static/css/2.b550b4be.chunk.css"
+  },
+  {
+    "revision": "60f55cd32da60480aa36",
+    "url": "/realtime-twitch/static/css/main.a93e6a82.chunk.css"
+  },
+  {
+    "revision": "e144150840eaf2ee2ad4",
+    "url": "/realtime-twitch/static/js/2.465e3073.chunk.js"
+  },
+  {
+    "revision": "60f55cd32da60480aa36",
+    "url": "/realtime-twitch/static/js/main.c37e99d2.chunk.js"
+  },
+  {
+    "revision": "cf42078f4d0c0a70ab46",
+    "url": "/realtime-twitch/static/js/runtime-main.3352742b.js"
+  }
+]);
